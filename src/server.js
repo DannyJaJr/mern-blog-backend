f5b4df226fdb618f488572a5b35ea0f7eccabce0
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 17:30:35 -0400

    server.js created

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+///endpoints
+app.get("/hello", (req, res) => res.send("Hello"));
+
+app.listen(8000, () => console.log("Listening on port 8000"));
