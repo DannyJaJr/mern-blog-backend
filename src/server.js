82998896dec9b3448614049079effe51f4457556
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 18:47:59 -0400

    routes added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+
 
 const app = express();
 app.use(express.json());
@@ -7,13 +7,35 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+const articlesInfo = {
+    'learn-react': {
+        comments: []
+    },
+
+    'learn-node': {
+        comments: [],
+    },
+
+    'my-thoughts-on-react': {
+        comments: [],
+    },
+}
+
 
 
 
 ///endpoints
-app.get("/hello", (req, res) => res.send("Hello"));
-app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
+// app.get("/hello", (req, res) => res.send("Hello"));
+// app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
+// app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
+app.post("/api/articles/:name/add-comments", (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+    res.status(200).send(articlesInfo[articleName]);
+
+});
 
 
 app.listen(8000, () => console.log("Listening on port 8000"));
