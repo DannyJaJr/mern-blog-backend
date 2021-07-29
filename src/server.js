15d8fac01dd7471ea2a24b494dc8ba58dbc5b4ef
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 17:55:27 -0400

    request with params

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({
 ///endpoints
 app.get("/hello", (req, res) => res.send("Hello"));
 app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
 
 
 app.listen(8000, () => console.log("Listening on port 8000"));
