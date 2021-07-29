5a46ff05952e6ad55f22c9cd26e4d5ab3cdb6066
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-29 17:49:20 -0400

    post route created

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+
 
 ///endpoints
 app.get("/hello", (req, res) => res.send("Hello"));
+app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
+
 
 app.listen(8000, () => console.log("Listening on port 8000"));
